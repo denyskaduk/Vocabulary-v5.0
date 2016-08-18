75ab896083e6c23865cebd405931d04798b2441d
Anna Ostropolets <aostropolets@sciforce.solutions>
2016-08-18 16:58:31 +0300

    update da_france ds_stage

--- DA_France/ds_stage_impovement.sql
+++ DA_France/ds_stage_impovement.sql
@@ -44,6 +44,31 @@ set DENOMINATOR_VALUE=null,
     DENOMINATOR_UNIT=null
 where DENOMINATOR_UNIT is not null and NUMERATOR_UNIT is null;
 
+--updating concepts that have amount_value but no  amount_unit
+update ds_stage 
+set amount_value=amount_value/1000,
+    amount_unit='MG'
+  where ingredient_concept_code in  (
+select ingredient_concept_code from   ds_stage s join france on drug_concept_Code=pfc
+where AMOUNT_VALUE is not null and AMOUNT_UNIT is null
+and (MOLECULE like '%NORELGESTROMIN%' or MOLECULE like '%ESTRADIOL%'  or MOLECULE like '%FLUTICASONE%'   or MOLECULE like '%BUDESONIDE%')
+)
+and amount_unit is null and AMOUNT_VALUE is not null
+;
+update ds_stage
+set     amount_unit='MG'
+where AMOUNT_VALUE is not null and AMOUNT_UNIT is null;
+
+--units
+INSERT INTO RELATIONSHIP_TO_CONCEPT(  CONCEPT_CODE_1,  VOCABULARY_ID_1,  CONCEPT_ID_2,  PRECEDENCE,CONVERSION_FACTOR) VALUES(  'TU',  'DA_France',  8510,  1,  1);
+INSERT INTO RELATIONSHIP_TO_CONCEPT(  CONCEPT_CODE_1,  VOCABULARY_ID_1,  CONCEPT_ID_2,PRECEDENCE,  CONVERSION_FACTOR) VALUES(  'TU',  'DA_France',  8718,  2,  1);
+INSERT INTO RELATIONSHIP_TO_CONCEPT(  CONCEPT_CODE_1,  VOCABULARY_ID_1, CONCEPT_ID_2,  PRECEDENCE,  CONVERSION_FACTOR)VALUES(  'CH',  'DA_France',  9324,  1,  1);
+INSERT INTO RELATIONSHIP_TO_CONCEPT(  CONCEPT_CODE_1,  VOCABULARY_ID_1,  CONCEPT_ID_2, PRECEDENCE,  CONVERSION_FACTOR)VALUES(  'MO',  'DA_France',  9573,  1,  1000);
+
+UPDATE RELATIONSHIP_TO_CONCEPT   SET CONCEPT_ID_2 = 35604680 WHERE CONCEPT_CODE_1 = 'OMOP6155' AND   CONCEPT_ID_2 = 561401 AND   PRECEDENCE = 1;
+UPDATE RELATIONSHIP_TO_CONCEPT   SET CONCEPT_ID_2 = 1759842 WHERE CONCEPT_CODE_1 = 'OMOP2713' AND   CONCEPT_ID_2 = 19046939 AND   PRECEDENCE = 1;
+
+
 
 
 commit
