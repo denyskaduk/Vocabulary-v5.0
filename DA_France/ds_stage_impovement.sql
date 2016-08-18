be99b50da23afc0c14ed92a6fb9277f6bfbf84c8
Anna Ostropolets <aostropolets@sciforce.solutions>
2016-08-18 15:52:10 +0300

    update da_france ds_stage

--- DA_France/ds_stage_impovement.sql
+++ DA_France/ds_stage_impovement.sql
@@ -26,5 +26,25 @@ and
  regexp_substr (DESCR_PCK, '[[:digit:]\.]+(ML|G)$') != replace (regexp_substr (DESCR_PCK, '/[[:digit:]\.]*(ML|G)| [[:digit:]\.]+(ML|G) ') , '/') 
 )) a where a.drug_concept_code = ds.drug_concept_code)
 ;
+
+--adding lost dosage
+update ds_stage 
+set numerator_unit=(select STRG_MEAS from france where pfc=drug_concept_Code and STRG_MEAS!='NULL')     ,
+    numerator_Value=(select  cast((STRG_UNIT) as number) from france where pfc=drug_concept_Code and STRG_UNIT!='NULL')  
+where DENOMINATOR_UNIT is not null and NUMERATOR_UNIT is null
+;
+--removing %
+update ds_stage
+set numerator_value=numerator_value*10*DENOMINATOR_VALUE,
+    numerator_unit='mg'
+where NUMERATOR_UNIT='%';
+
+update ds_stage 
+set DENOMINATOR_VALUE=null,
+    DENOMINATOR_UNIT=null
+where DENOMINATOR_UNIT is not null and NUMERATOR_UNIT is null;
+
+
+
 commit
 ;
